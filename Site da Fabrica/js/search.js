a54dcbda22178b6773063905cd3de29acf73684b
Phablinhoobscuro <phabloescobarrcorreia@gmail.com>
2025-09-13 13:43:46 -0300

    .

--- Site da Fabrica/js/search.js
+++ Site da Fabrica/js/search.js
@@ -1,5 +1,11 @@
-// search.js
-export function inicializarBusca(inputId, suggestionsId) {
+
+export async function inicializarBusca(inputId, suggestionsId) {
+
+  const res = await fetch("http://localhost:8080/config/config");
+  const data = await res.json();
+ 
+
+
   const input = document.getElementById(inputId);
   const suggestions = document.getElementById(suggestionsId);
 
@@ -7,16 +13,15 @@ export function inicializarBusca(inputId, suggestionsId) {
 
   input.addEventListener("input", () => {
     const value = input.value.trim().toLowerCase();
+    
     suggestions.innerHTML = "";
 
     if (value.length > 0) {
-      const results = ["Projeto A", "Projeto B", "Membro X"].filter(item =>
-        item.toLowerCase().includes(value)
-      );
-
+      const itesns = data.map(item => item.codigoProjeto);
+      console.log(itesns)
       results.forEach(result => {
         const item = document.createElement("button");
-        item.className = "list-group-item list-group-item-action";
+        item.nomeDoProjeto = "list-group-item list-group-item-action";
         item.textContent = result;
         suggestions.appendChild(item);
       });
